5c4965a7805f78139325c82e859f4199ee251ffd
DILIF-PROJECT <ddiilliiff@hotmail.com>
2023-12-20 02:22:51 +0900

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -6,4 +7,15 @@ void main() {
   runApp(GameWidget(game: MyApp()));
 }
 
-class MyApp extends FlameGame {}
+class MyApp extends FlameGame {
+  SpriteComponent backgroud = SpriteComponent();
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    backgroud
+      ..sprite = await loadSprite('background/1.png')
+      ..size = Vector2(732, 412);
+    add(backgroud);
+  }
+}
